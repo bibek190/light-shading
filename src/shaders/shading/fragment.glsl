ce31ab0e53497d4d9b996bd3256a84c3d32f2540
bibek190 <bibek9818917036@gmail.com>
2025-01-27 12:22:49 +1100

    applying ambient light to the scene

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,8 +1,16 @@
 uniform vec3 uColor;
 
+vec3 ambientLight(vec3 lightColor,float lightIntensity){
+    return lightColor * lightIntensity;
+}
+
 void main()
 {
     vec3 color = uColor;
+
+    vec3 light = vec3(0.0);
+    light += ambientLight(vec3(1.0),0.2);
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
