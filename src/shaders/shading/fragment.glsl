eca2c019e3a83173bab87fd8fd019ae8560f0fd2
bibek190 <bibek9818917036@gmail.com>
2025-01-28 11:20:18 +1100

    add ambient light and directional lights

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,15 +1,51 @@
 uniform vec3 uColor;
 
-vec3 ambientLight(vec3 lightColor,float lightIntensity){
-    return lightColor * lightIntensity;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+
+#include ../includes/ambientLight.glsl;
+
+vec3 directionalLight(vec3 lightColor,float lightIntensity,vec3 normal,vec3 lightPosition,vec3 viewDirection){
+    
+    vec3 lightDirection = normalize(lightPosition);
+    vec3 lightReflection = reflect(-lightDirection,normal);
+    
+    // shading 
+    float shading = dot(normal,lightDirection);
+    shading = max(0.0,shading);
+
+    // Specular 
+    float specular = dot(lightReflection,viewDirection);
+    specular = pow(specular,20.0);
+
+
+    // return lightColor * lightIntensity * shading;
+    return vec3(specular);
 }
+
 
 void main()
 {
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 color = uColor;
 
+    // Light 
     vec3 light = vec3(0.0);
-    light += ambientLight(vec3(1.0),0.2);
+    light += ambientLight(
+        vec3(1.0),    //LightColor
+        0.03         //LightIntensity
+        );
+
+    // directionalLight
+light += directionalLight(
+    vec3(0.1,0.1,1.0),      //lightColor
+    1.0,             //light intensity
+    vNormal,         // normal
+    vec3(0.0,0.0,3.0),      //light position
+    viewDirection
+    );
+
     color *= light;
 
     // Final color
