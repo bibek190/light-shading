6bfef1bf53d7dedd6de2764187f5557272692f2a
bibek190 <bibek9818917036@gmail.com>
2025-01-29 02:03:04 +1100

    added

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,52 +1,34 @@
 uniform vec3 uColor;
 
 varying vec3 vNormal;
-varying vec3 vPosition;
 
 
 #include ../includes/ambientLight.glsl;
 
-vec3 directionalLight(vec3 lightColor,float lightIntensity,vec3 normal,vec3 lightPosition,vec3 viewDirection){
-    
-    vec3 lightDirection = normalize(lightPosition);
-    vec3 lightReflection = reflect(-lightDirection,normal);
-    
-    // shading 
-    float shading = dot(normal,lightDirection);
-    shading = max(0.0,shading);
-
-    // Specular 
-    float specular = dot(lightReflection,viewDirection);
-    specular = pow(specular,20.0);
-
-
-    // return lightColor * lightIntensity * shading;
-    return vec3(specular);
+vec3 directionalLight(vec3 lightColor,float lightIntensity,vec3 normal,vec3 lightPosition){
+    return lightColor * lightIntensity * normal ;
 }
-
 
 void main()
 {
-    vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 color = uColor;
 
-    // Light 
     vec3 light = vec3(0.0);
-    light += ambientLight(
-        vec3(1.0),    //LightColor
-        0.03         //LightIntensity
-        );
+    // light += ambientLight(
+    //     vec3(1.0),         //lightColor
+    //     0.03               // light intensity
+    //     );
 
-    // directionalLight
 light += directionalLight(
-    vec3(0.1,0.1,1.0),      //lightColor
-    1.0,             //light intensity
-    vNormal,         // normal
-    vec3(0.0,0.0,3.0),      //light position
-    viewDirection
-    );
+    vec3(0.0,0.1,1.0)
+    ,1.0,
+    vNormal,vec3(0.0,0.0,3.0))
+    ;
 
     color *= light;
+
+
+
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
--- src/shaders/shading/vertex.glsl
+++ src/shaders/shading/vertex.glsl
@@ -1,5 +1,4 @@
 varying vec3 vNormal;
-varying vec3 vPosition;
 
 void main()
 {
@@ -7,12 +6,7 @@ void main()
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
-// Model normal
-vec4 modelNormal = modelMatrix * vec4(normal,0.0);
 
-
-// Varying
-    vNormal = modelNormal.xyz;
-    vPosition = position.xyz;
-    
+    // varying
+    vNormal = normal;
 }
